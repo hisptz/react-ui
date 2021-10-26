e64020f4b1f7e7bf91ce1a08f8a8bf2ba205be82
Danford Kija <kijadanford@gmail.com>
2021-10-26 18:00:55 +0300

    intergrate:chart item into chartanalytics

--- src/components/ChartAnalytics/components/components/chart-item/chart-item-component.tsx
+++ src/components/ChartAnalytics/components/components/chart-item/chart-item-component.tsx
@@ -1,39 +1,112 @@
 /* eslint-disable max-params */
 /* eslint-disable no-unused-vars */
-import HighCharts from 'highcharts';
-import * as _ from 'lodash';
-import React, { useEffect, useState } from 'react';
-import { getCharObject } from '../../../helper/get-chart-object.helper';
-import { ChartConfigurationProps } from 'components/ChartAnalytics/interfaces/props';
+import HighCharts from "highcharts";
+import * as _ from "lodash";
+import React, { useEffect, useState } from "react";
+import { getCharObject } from "../../../helper/get-chart-object.helper";
+import { getChartConfiguration } from "components/ChartAnalytics/helper/get-chart-configuration.helper";
+import { ChartConfigurationProps } from "components/ChartAnalytics/interfaces/props";
+import { CHART_TYPES } from "core/constant/chart-types.constant";
 
-export default function ChartItemComponent(analysisData:any,chartHeight: number,analyticsObject: any, chartConfiguration:ChartConfigurationProps){
+export default function ChartItemComponent(
+  analysisData: any,
+  chartHeight: number,
+  analyticsObject: any,
+  chartConfiguration: ChartConfigurationProps
+) {
+  const [drawChartConfiguration] = useState(
+    chartConfigurationSelector(
+      chartConfiguration.layout,
+      chartConfiguration.currentChartType
+    )
+  );
+  const [chartTypes] = useState(CHART_TYPES);
+  const [currentChartType, setCurrentChartType] = useState("column");
+  const [chartUpdate, setChartUpdate] = useState({
+    id: "",
+    type: "",
+  });
 
+  let chart: any = "";
 
- const [drawChartConfiguration] = useState({"d":"d"});
- 
- let chart:any = '';
+  useEffect(() => {
+    drawChart(analysisData["_data"], drawChartConfiguration);
+  }, [
+    analysisData,
+    chartConfiguration.layout,
+    chartConfiguration.currentChartType,
+  ]);
 
+  function drawChart(analyticsObject: any, drawChartConfiguration: any) {
+    if (drawChartConfiguration && analyticsObject) {
+      const chartObject = getCharObject(
+        analyticsObject,
+        drawChartConfiguration
+      );
+      if (chartObject) {
+        setTimeout(() => {
+          chart = HighCharts.chart("renderId", chartObject);
+        }, 20);
+      }
+    }
+  }
+  function chartConfigurationSelector(layout: any, currentChartType: any) {
+    const configuration = getChartConfiguration(
+      {},
+      "render-id-unique",
+      layout,
+      "",
+      currentChartType,
+      []
+    );
+    return configuration;
+  }
 
-useEffect(() => {
-    drawChart(analysisData['_data'], drawChartConfiguration);  
-  
-}, [analysisData,chartConfiguration.layout,chartConfiguration.currentChartType]);
+  function updateChartType(chartType: any, event: any) {
+    event.stopPropagation();
+    setCurrentChartType(chartType);
+    drawChart(analysisData["_data"], {
+      ...chartConfiguration,
+      type: chartType,
+    });
 
- function drawChart(analyticsObject:any, drawChartConfiguration:any){
-     if(drawChartConfiguration && analyticsObject ){
-            const chartObject = getCharObject(analyticsObject, drawChartConfiguration);
-            if(chartObject){
-                setTimeout(()=>{
-chart = HighCharts.chart("renderId",chartObject)
-                },20)
-            }
+    setChartUpdate({
+      id: "render-id-unique",
+      type: chartType.toUpperCase,
+    });
+  }
 
-     }
- }
+  return (
+    <div className="chart-item-container" style={{ width: "100%" }}>
+      <div
+        id="renderId"
+        className="chart-block"
+        style={{ height: "calc(" + chartHeight + "px-20px", width: "100%" }}
+      ></div>
 
-     return (
-         <div>
-
-         </div>
-     );
+      <ul className="chart-type-list animated fadeInRight">
+        {chartTypes?.map((chartType, chartTypePosition) => {
+          return (
+            <li key={"chart-type" + chartTypePosition}>
+              <button
+                onClick={(e) => updateChartType(chartType.type, e)}
+                title={chartType.description}
+                className={
+                  chartConfiguration.currentChartType == chartType.type
+                    ? "active-chart-type"
+                    : ""
+                }
+              >
+                <img
+                  src={chartType.icon}
+                  className="chart-option-icon"
+                  alt=""
+                />
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
--- src/components/ChartAnalytics/components/index.tsx
+++ src/components/ChartAnalytics/components/index.tsx
@@ -1,11 +1,17 @@
+/* eslint-disable max-params */
 import React, { Suspense } from 'react';
+import { ChartConfigurationProps } from '../interfaces/props';
 import '../styles/index.css';
+import ChartItemComponent from './components/chart-item/chart-item-component';
 
-export default function ChartAnalytics({}){
+export default function ChartAnalytics( analysisData: any,
+  chartHeight: number = 2000,
+  analyticsObject: any,
+  chartConfiguration: ChartConfigurationProps){
     return <div>
          <div className="chart-list">
       <Suspense fallback={<div>Loading .....</div>}>
-        {/* <ChartItemComponent chartHeight={2000} /> */}
+        <ChartItemComponent analysisDat={analysisData} chartHeight={chartHeight} analyticsObject={analyticsObject} chartConfiguration={chartConfiguration}/>
       </Suspense>
     </div>
     </div>
