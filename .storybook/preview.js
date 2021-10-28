c540935cd8f85fad66b93c4b67506888060c7871
Gift Nnko <nnkogift@gmai.com>
2021-10-28 17:01:53 +0300

    refactor(Utils Location): Refactor
    - setup storybook login to dhis2

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,6 @@
 import { Provider } from "@dhis2/app-runtime";
 import { CenteredContent, CircularLoader } from "@dhis2/ui";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -17,8 +17,42 @@ const appConfig = {
   apiVersion: parseInt(process.env.STORYBOOK_DHIS2_API_VERSION ?? "") ?? 36,
 };
 
+const loginUrl = `${process.env.STORYBOOK_DHIS2_BASE_URL}/dhis-web-commons-security/login.action`;
+
 function useLogin() {
-  const [loading] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function login() {
+      setLoading(true);
+      const data = [
+        `${encodeURIComponent("j_username")}=${encodeURIComponent(process.env.STORYBOOK_DHIS2_USERNAME)}`,
+        `${encodeURIComponent("j_password")}=${encodeURIComponent(process.env.STORYBOOK_DHIS2_PASSWORD)}`,
+      ].join("&");
+      try {
+        const response = await fetch(loginUrl, {
+          method: "POST",
+          redirect: "follow",
+          credentials: "include",
+          mode: "no-cors",
+          headers: {
+            Accept: "*",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Access-Control-Allow-Credentials": true,
+            "Access-Control-Allow-Origin": process.env.STORYBOOK_DHIS2_BASE_URL,
+          },
+          body: data,
+        });
+        console.log(response);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    }
+
+    login();
+  }, []);
+
   return { loading };
 }
 
--- src/components/ChartAnalytics/components/chart-item/chart-item-component.tsx
+++ src/components/ChartAnalytics/components/chart-item/chart-item-component.tsx
@@ -1,79 +1,47 @@
 /* eslint-disable max-params */
 /* eslint-disable no-unused-vars */
 import HighCharts from "highcharts";
-import * as _ from "lodash";
-import React, { useEffect, useMemo, useState } from "react";
+import HighchartsReact from "highcharts-react-official";
+import React, { useEffect, useState } from "react";
 import { getCharObject } from "../../helper/get-chart-object.helper";
 import { getChartConfiguration } from "components/ChartAnalytics/helper/get-chart-configuration.helper";
-import { ChartAnalyticsProps, ChartConfigurationProps } from "components/ChartAnalytics/types/props";
+import { ChartAnalyticsProps } from "components/ChartAnalytics/types/props";
 import { CHART_TYPES } from "core/constant/chart-types.constant";
-import './chart-item-component.css';
-import { uid } from "core/utils";
+import "./chart-item-component.css";
 
-export default function ChartItemComponent(
-{  analysisData,
-  chartHeight,
-  chartConfiguration}:ChartAnalyticsProps
-) {
- 
+export default function ChartItemComponent({ analysisData, chartHeight, chartConfiguration }: ChartAnalyticsProps) {
   const [chartTypes] = useState(CHART_TYPES);
   const [currentChartType, setCurrentChartType] = useState(chartConfiguration.currentChartType);
+  const [chartOptions, setChartOptions] = useState();
   const [chartUpdate, setChartUpdate] = useState({
     id: "",
     type: "",
   });
 
-  let chart: any = "";
+  const [drawChartConfiguration, setDrawChartConfiguration] = useState(chartConfigurationSelector(chartConfiguration.layout, currentChartType));
 
-  const [drawChartConfiguration,setDrawChartConfiguration] = useState(
-    chartConfigurationSelector(
-      chartConfiguration.layout,
-      currentChartType
-    )
-  );
-
-  useEffect(() => {   
+  useEffect(() => {
     drawChart(analysisData["_data"], drawChartConfiguration);
-  }, [
-    analysisData,
-    chartConfiguration.layout,
-    currentChartType,
-  ]);
-  const renderId:any = useMemo(() => uid(), [])
+  }, [analysisData, chartConfiguration.layout, currentChartType]);
 
   function drawChart(analyticsObject: any, drawChartConfiguration: any) {
     if (drawChartConfiguration && analyticsObject) {
-      const chartObject = getCharObject(
-        analyticsObject,
-        drawChartConfiguration
-      );
+      const chartObject = getCharObject(analyticsObject, drawChartConfiguration);
       if (chartObject) {
-        setTimeout(() => {
-          chart = HighCharts.chart(renderId, chartObject);
-        }, 20);
+        setChartOptions(chartObject);
       }
     }
   }
+
   function chartConfigurationSelector(layout: any, currentChartType: any) {
-    const configuration = getChartConfiguration(
-      {},
-      "render-id-unique",
-      layout,
-      "",
-      currentChartType,
-      []
-    );
-    return configuration;
+    return getChartConfiguration({}, "", layout, "", currentChartType, []);
   }
 
   function updateChartType(chartType: any, event: any) {
     event.stopPropagation();
-    
+
     setCurrentChartType(chartType);
-    setDrawChartConfiguration( chartConfigurationSelector(
-      chartConfiguration.layout,
-      chartType
-    ))
+    setDrawChartConfiguration(chartConfigurationSelector(chartConfiguration.layout, chartType));
     drawChart(analysisData["_data"], {
       ...drawChartConfiguration,
       type: chartType,
@@ -88,30 +56,22 @@ export default function ChartItemComponent(
   return (
     <div className="chart-item-container" style={{ width: "100%" }}>
       <div
-        id={renderId}        
         className="chart-block"
-        style={{ height: "calc(" + chartHeight + "px-20px", width: "100%" }}
-      ></div>
-
-      <ul 
-                  id="dataImg" className="chart-type-list animated fadeInRight">
+        style={{
+          height: "calc(" + chartHeight + "px-20px",
+          width: "100%",
+        }}>
+        <HighchartsReact highcharts={HighCharts} options={chartOptions} />
+      </div>
+      <ul id="dataImg" className="chart-type-list animated fadeInRight">
         {chartTypes?.map((chartType, chartTypePosition) => {
           return (
             <li key={"chart-type" + chartTypePosition}>
               <button
                 onClick={(e) => updateChartType(chartType.type, e)}
                 title={chartType.description}
-                className={
-                  chartConfiguration.currentChartType == chartType.type
-                    ? "active-chart-type"
-                    : ""
-                }
-              >
-                <img
-                  src={chartType.icon}
-                  className="chart-option-icon"
-                  alt=""
-                />
+                className={chartConfiguration.currentChartType == chartType.type ? "active-chart-type" : ""}>
+                <img src={chartType.icon} className="chart-option-icon" alt="" />
               </button>
             </li>
           );
