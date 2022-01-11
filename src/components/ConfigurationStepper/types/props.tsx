8790aaf3564bf71b476582b526c135f4026f82ba
Gift Nnko <nnkogift@hotmail.com>
2022-01-11 10:16:54 +0300

    feat(Stepper ): Implementation
    - Added onStepChange handler to allow blocking of step change

--- src/components/ConfigurationStepper/types/props.tsx
+++ src/components/ConfigurationStepper/types/props.tsx
@@ -1,16 +1,16 @@
-
 export type StepsList = {
-    label:string,
-    component:(props?:any)=>any,
-    helpSteps?:Array<any>
-}
+  label: string;
+  component: (props?: any) => any;
+  helpSteps?: Array<any>;
+};
 
-export type ConfigurationStepperProps ={
-    stepsManagement:StepsList[],
-    onLastAction:(value?:any) => void,
-    activeStepperBackGroundColor:string,
-    onCancelLastAction: (value?:any)=>void,
-    onLastActionButtonName:string
-
-
-}
+export type ConfigurationStepperProps = {
+  stepsManagement: StepsList[];
+  onLastAction: (value?: any) => void;
+  activeStepperBackGroundColor: string;
+  onCancelLastAction: (value?: any) => void;
+  onLastActionButtonName: string;
+  onNextStep?: (fromIndex: number, toIndex: number) => boolean;
+  onPreviousStep?: (fromIndex: number, toIndex: number) => boolean;
+  onStepChange?: (fromIndex: number, toIndex: number) => boolean;
+};
