3c60c79e1f9516a9e09ed01f9d0f080460f55791
Gift Nnko <nnkogift@hotmail.com>
2022-01-11 12:14:25 +0300

    feat(Stepper): Improvements
    - Made the onNextStep function async to allow async validations

--- src/components/ConfigurationStepper/types/props.tsx
+++ src/components/ConfigurationStepper/types/props.tsx
@@ -10,7 +10,5 @@ export type ConfigurationStepperProps = {
   activeStepperBackGroundColor: string;
   onCancelLastAction: (value?: any) => void;
   onLastActionButtonName: string;
-  onNextStep?: (fromIndex: number, toIndex: number) => boolean;
-  onPreviousStep?: (fromIndex: number, toIndex: number) => boolean;
-  onStepChange?: (fromIndex: number, toIndex: number) => boolean;
+  onStepChange?: (fromIndex: number, toIndex: number) => boolean | Promise<boolean>;
 };
