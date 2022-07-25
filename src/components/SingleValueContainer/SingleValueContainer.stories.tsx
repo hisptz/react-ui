8742a4ded3c760470717cead122152456332b313
megamind <echingalo@gmail.com>
2022-07-25 18:11:08 +0300

    docs: ✏️ added storybook docs

--- src/components/SingleValueContainer/SingleValueContainer.stories.tsx
+++ src/components/SingleValueContainer/SingleValueContainer.stories.tsx
@@ -3,20 +3,47 @@ import React from "react";
 import { SingleValueContainerProps } from "./types/props";
 import SingleValueContainer from ".";
 
-const Template: Story<SingleValueContainerProps> = (args) => <SingleValueContainer />;
+const Template: Story<SingleValueContainerProps> = (args) => <SingleValueContainer {...args} />;
 
 export const Default = Template.bind({});
+Default.args = {
+  title: "PRIORITY INDICATORS",
+  singleValueItems: [
+    {
+      label: "Total Bookings",
+      value: 136,
+    },
+    {
+      label: "At least one dose",
+      value: 45,
+      percentage: 23,
+      color: "#0D47A1",
+    },
+    {
+      label: "Partially vaccinated",
+      value: 13,
+      percentage: 17,
+      color: "#0D47A1",
+    },
+    {
+      label: "Fully vaccinated",
+      value: 126,
+      percentage: 83,
+      color: "#0D47A1",
+    },
+    {
+      label: "Number of AEFI",
+      value: 26,
+    },
+  ],
+};
 
 export default {
   title: "Components/Single Value Container",
   component: SingleValueContainer,
   decorators: [
     (SingleValueStory: any) => {
-      return (
-        <div style={{ width: 900, height: "100%" }}>
-          <SingleValueStory />
-        </div>
-      );
+      return <SingleValueStory />;
     },
   ],
 };
