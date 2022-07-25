3aec6621c5fc79b7834b5162179cbbebed471c4a
megamind <echingalo@gmail.com>
2022-07-25 18:09:17 +0300

    refactor: 💡 improved the single value container
    added the respective value, label and percentage progress

--- src/components/SingleValueContainer/index.tsx
+++ src/components/SingleValueContainer/index.tsx
@@ -1,5 +1,22 @@
-import React from "react";
+import i18n from "@dhis2/d2-i18n";
+import React, { Suspense } from "react";
+import SingleValueItem from "./components/SingleValueItem/SingleValueItem";
+import { SingleValue, SingleValueContainerProps } from "./types/props";
+import "./styles/index.css";
 
-export default function SingleValueContainer(): React.ReactElement {
-  return <div>Hello Word</div>;
+export default function SingleValueContainer({ title, singleValueItems }: SingleValueContainerProps): React.ReactElement {
+  return (
+    <div className="w-100 h-100">
+      <Suspense fallback={<div>{i18n.t("Loading ...")}</div>}>
+        <div>
+          <span className="font-bold font-x-large">{title}</span>
+          <div className="single-value-list">
+            {singleValueItems.map((singleValueItem: SingleValue) => (
+              <SingleValueItem key={`${singleValueItem.label}-${singleValueItem.value}`} {...singleValueItem} />
+            ))}
+          </div>
+        </div>
+      </Suspense>
+    </div>
+  );
 }
