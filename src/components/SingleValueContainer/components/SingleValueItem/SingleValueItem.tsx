8cb3cf3ac61f1f389f673865b0daa5b804508fba
gnnko <nnkogift@hotmail.com>
2022-07-26 11:37:50 +0300

    :bug: fix(single value container): Fixes
    - Modularized css
     - Added Css reset

--- src/components/SingleValueContainer/components/SingleValueItem/SingleValueItem.tsx
+++ src/components/SingleValueContainer/components/SingleValueItem/SingleValueItem.tsx
@@ -1,13 +1,13 @@
 import React from "react";
+import styles from "../../styles/SingleValueContainer.module.css";
 import { SingleValue } from "../../types/props";
-import "../../styles/index.css";
 import SingleValuePercentage from "./SingleValuePercentage";
 
 export default function SingleValueItem({ label, value, color, percentage }: SingleValue): React.ReactElement {
   return (
-    <div className="single-value-item text-center">
-      <div className="font-large">{label}</div>
-      <div className="font-bold font-xx-large">{value}</div>
+    <div className={`${styles["single-value-item"]} text-center`}>
+      <div className={styles["font-large"]}>{label}</div>
+      <div className={`${styles["font-bold"]} ${styles["font-xx-large"]}`}>{value}</div>
       {percentage ? <SingleValuePercentage color={color} percentage={percentage} /> : <span></span>}
     </div>
   );
--- src/components/SingleValueContainer/index.tsx
+++ src/components/SingleValueContainer/index.tsx
@@ -1,16 +1,18 @@
 import i18n from "@dhis2/d2-i18n";
+import { CssReset } from "@dhis2/ui";
 import React, { Suspense } from "react";
 import SingleValueItem from "./components/SingleValueItem/SingleValueItem";
+import styles from "./styles/SingleValueContainer.module.css";
 import { SingleValue, SingleValueContainerProps } from "./types/props";
-import "./styles/index.css";
 
 export default function SingleValueContainer({ title, singleValueItems }: SingleValueContainerProps): React.ReactElement {
   return (
     <div className="w-100 h-100">
+      <CssReset />
       <Suspense fallback={<div>{i18n.t("Loading ...")}</div>}>
         <div>
-          <span className="font-bold font-x-large">{title}</span>
-          <div className="single-value-list">
+          <span className={`${styles["font-x-large"]} ${styles["font-bold"]}`}>{title}</span>
+          <div className={styles["single-value-list"]}>
             {singleValueItems.map((singleValueItem: SingleValue) => (
               <SingleValueItem key={`${singleValueItem.label}-${singleValueItem.value}`} {...singleValueItem} />
             ))}
