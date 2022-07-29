0ad2bed2c129f46ad0635ffda22deb4330fce5e0
gnnko <nnkogift@hotmail.com>
2022-07-29 16:26:49 +0300

    chore(index): Export
    - Exported new chart components and utils

--- src/components/ChartAnalytics/index.tsx
+++ src/components/ChartAnalytics/index.tsx
@@ -2,9 +2,13 @@ import { uid } from "@hisptz/dhis2-utils";
 import HighCharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 import React, { forwardRef, useRef } from "react";
+import ChartDownloadMenu from "./components/DownloadMenu";
 import { useChart } from "./hooks/useChart";
 import { ChartAnalyticsProps } from "./types/props";
 import "./styles/custom-highchart.css";
+
+export * from "./services/export";
+export { ChartDownloadMenu };
 
 function ChartAnalytics({ analytics, config }: ChartAnalyticsProps, ref: React.ForwardedRef<HighchartsReact.RefObject>) {
   const id = useRef(`${uid()}-chart-item`);
