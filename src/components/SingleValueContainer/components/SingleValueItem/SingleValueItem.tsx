62c3f79505ce46152311a0a27225f30d8e0e5404
megamind <echingalo@gmail.com>
2022-08-04 07:02:54 +0300

    feat: 🎸 abbreviated the values on the single value component

--- src/components/SingleValueContainer/components/SingleValueItem/SingleValueItem.tsx
+++ src/components/SingleValueContainer/components/SingleValueItem/SingleValueItem.tsx
@@ -1,13 +1,20 @@
+import { Tooltip } from "@dhis2/ui";
 import React from "react";
+import { VALUE_TYPES } from "../../../FormField/constants";
 import styles from "../../styles/SingleValueContainer.module.css";
 import { SingleValue } from "../../types/props";
 import SingleValuePercentage from "./SingleValuePercentage";
 
+const numberFormatter = (value: number) => Intl.NumberFormat("en", { notation: "compact" }).format(value);
+
 export default function SingleValueItem({ label, value, color, percentage }: SingleValue): React.ReactElement {
+  const sanitizedValue = typeof value == "string" ? value : numberFormatter(value);
   return (
     <div className={`${styles["single-value-item"]} text-center`}>
       <div className={styles["font-large"]}>{label}</div>
-      <div className={`${styles["font-bold"]} ${styles["font-xx-large"]} ${styles["padding-top"]}`}>{value}</div>
+      <Tooltip content={value}>
+        <div className={`${styles["font-bold"]} ${styles["font-xx-large"]} ${styles["padding-top"]}`}>{sanitizedValue}</div>
+      </Tooltip>
       {percentage ? <SingleValuePercentage color={color} percentage={percentage} /> : <span></span>}
     </div>
   );
