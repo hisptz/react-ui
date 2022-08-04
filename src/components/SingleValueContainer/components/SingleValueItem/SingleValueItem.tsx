778c58350c9eca1551528ef2dfffc92b961aae7e
megamind <echingalo@gmail.com>
2022-08-04 11:01:36 +0300

    feat: 🎸 added animation of the single values

--- src/components/SingleValueContainer/components/SingleValueItem/SingleValueItem.tsx
+++ src/components/SingleValueContainer/components/SingleValueItem/SingleValueItem.tsx
@@ -1,19 +1,40 @@
 import { Tooltip } from "@dhis2/ui";
 import React from "react";
-import { VALUE_TYPES } from "../../../FormField/constants";
+import { useSpring, animated } from "react-spring";
 import styles from "../../styles/SingleValueContainer.module.css";
 import { SingleValue } from "../../types/props";
 import SingleValuePercentage from "./SingleValuePercentage";
 
-const numberFormatter = (value: number) => Intl.NumberFormat("en", { notation: "compact" }).format(value);
+interface SingleValueProps extends SingleValue {
+  globalAnimationDelay?: number;
+  globalAnimationDuration?: number;
+}
 
-export default function SingleValueItem({ label, value, color, percentage }: SingleValue): React.ReactElement {
-  const sanitizedValue = typeof value == "string" ? value : numberFormatter(value);
+const numberFormatter = (value: number) => Intl.NumberFormat("en-US", { notation: "compact" }).format(value);
+
+export default function SingleValueItem({
+  label,
+  value,
+  color,
+  percentage,
+  animationDuration,
+  animationDelay,
+  globalAnimationDelay,
+  globalAnimationDuration,
+}: SingleValueProps): React.ReactElement {
+  const sanitizedValue = useSpring({
+    val: value,
+    from: { val: 0 },
+    config: { duration: animationDuration ?? globalAnimationDuration ?? 1000 },
+    delay: animationDelay ?? globalAnimationDelay ?? 10,
+  });
   return (
     <div className={`${styles["single-value-item"]} text-center`}>
       <div className={styles["font-large"]}>{label}</div>
       <Tooltip content={value}>
-        <div className={`${styles["font-bold"]} ${styles["font-xx-large"]} ${styles["padding-top"]}`}>{sanitizedValue}</div>
+        <animated.div className={`${styles["font-bold"]} ${styles["font-xx-large"]} ${styles["padding-top"]}`}>
+          {sanitizedValue.val.to((value) => numberFormatter(Math.floor(value)))}
+        </animated.div>
       </Tooltip>
       {percentage ? <SingleValuePercentage color={color} percentage={percentage} /> : <span></span>}
     </div>
--- src/components/SingleValueContainer/types/props.tsx
+++ src/components/SingleValueContainer/types/props.tsx
@@ -1,11 +1,15 @@
 export type SingleValueContainerProps = {
   title: string;
   singleValueItems: Array<SingleValue>;
+  animationDuration?: number;
+  animationDelay?: number;
 };
 
 export type SingleValue = {
   label: string;
-  value: number | string;
+  value: number;
   percentage?: number;
   color?: string;
+  animationDuration?: number;
+  animationDelay?: number;
 };
