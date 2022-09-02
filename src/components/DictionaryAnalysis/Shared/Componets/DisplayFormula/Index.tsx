61f19f68c5b0d6f426841664b5cf9c31dbef57c2
Baraka Ally <baraka.ally.co.tz@gmail.com>
2022-09-02 12:56:51 +0300

    chore: removed unused files in DisplayFormula file

--- src/components/DictionaryAnalysis/Shared/Componets/DisplayFormula/Index.tsx
+++ src/components/DictionaryAnalysis/Shared/Componets/DisplayFormula/Index.tsx
@@ -1,52 +1,46 @@
 import { useDataEngine } from "@dhis2/app-runtime";
 import DictionaryContext from "../../../../../components/DictionaryAnalysis/Store/DictionaryContext";
 import PropTypes from "prop-types";
-import React, { useContext, useEffect, useState } from "react";
-// import { useSetRecoilState } from "recoil";
-//import { dataElementsStateDictionary, programIndicatorStateDictionary, } from "../../../Store";
-import { getDetailedValueFromApi, getFinalWordFormula, getFormulaSources, getWordData, } from "../../../Utils/Functions/FormulaFunctions";
+import { useContext, useEffect} from "react";
+import { getFinalWordFormula} from "../../../Utils/Functions/FormulaFunctions";
 import { useGetData } from "../../../Utils/Hooks";
 import { dataTypes } from "../../../Utils/Models";
 import Error from "../Error/ErrorAPIResult";
 import Loader from "../Loaders/Loader";
 
-export default function DisplayFormula(props:any) {
-    //props
-    const formula = props.formula;
-    const loc = props.location; //either its in numerator or denominator
-    const storeResult = props.storeResult;
-    
-   const {values,setValues}=useContext(DictionaryContext);
+export default function DisplayFormula(props: any) {
+  //props
+  const formula = props.formula;
+  const loc = props.location; //either its in numerator or denominator
+  const storeResult = props.storeResult;
 
-    //hooks
-   // const updateDataElementHandler = useSetRecoilState(dataElementsStateDictionary);
-    const engine = useDataEngine();
-    const { loading, error, data }:any = useGetData(formula, engine, loc);
+  const { values, setValues } = useContext(DictionaryContext);
 
-    useEffect(() => {
-        if (storeResult && typeof data?.dataElements != dataTypes.UNDEFINED) {
-            // updateDataElementHandler((prev:any) => {
-            //     return prev?.concat(data?.dataElements);
-            // });
+  //hooks
+  const engine = useDataEngine();
+  const { loading, error, data }: any = useGetData(formula, engine, loc);
 
-            setValues({...values,...{dataElements:values?.dataElements?.concat(data?.dataElements)}})
-        }
-    }, [data]);
-
-    if (loading) {
-        return <Loader text={""}/>;
+  useEffect(() => {
+    if (storeResult && typeof data?.dataElements != dataTypes.UNDEFINED) {
+      setValues({ ...values, ...{ dataElements: values?.dataElements?.concat(data?.dataElements) } });
     }
-    if (error) {
-        return <Error error={error}/>;
-    }
-    return (<div>
-      {getFinalWordFormula(formula, data?.dataElements, data?.programIndicators, data?.dataSetReportingRates, data?.attributes, data?.constants,null,null)}
-    </div>);
+  }, [data]);
+
+  if (loading) {
+    return <Loader text={""} />;
+  }
+  if (error) {
+    return <Error error={error} />;
+  }
+  return (
+    <div>
+      {getFinalWordFormula(formula, data?.dataElements, data?.programIndicators, data?.dataSetReportingRates, data?.attributes, data?.constants, null, null)}
+    </div>
+  );
 }
 
 DisplayFormula.PropTypes = {
-    formula: PropTypes.string.isRequired,
-    location: PropTypes.string,
-    storeResult: PropTypes.bool
+  formula: PropTypes.string.isRequired,
+  location: PropTypes.string,
+  storeResult: PropTypes.bool,
 };
-
