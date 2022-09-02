67178783f496dcd88c2a1d8dd3f4b2b6f35e7838
Baraka Ally <baraka.ally.co.tz@gmail.com>
2022-09-02 13:26:16 +0300

    core: clean calculationDetails

--- src/components/DictionaryAnalysis/Module/Indicator/Components/calculationDetails/Components/Row/index.tsx
+++ src/components/DictionaryAnalysis/Module/Indicator/Components/calculationDetails/Components/Row/index.tsx
@@ -2,64 +2,66 @@ import { useDataEngine, useDataQuery } from "@dhis2/app-runtime";
 import { CircularLoader, DataTableCell } from "@dhis2/ui";
 import DictionaryContext from "../../../../../../../../components/DictionaryAnalysis/Store/DictionaryContext";
 import PropTypes from "prop-types";
-import React, { useContext, useEffect, useState } from "react";
-// import { useSetRecoilState } from "recoil";
+import { useContext } from "react";
 import Error from "../../../../../../Shared/Componets/Error/ErrorAPIResult";
 import Loader from "../../../../../../Shared/Componets/Loaders/Loader";
-// import { dataElementsStateDictionary, dataSetReportingRatesStateDictionary, programIndicatorStateDictionary, } from "../../../../../../Store";
-import { getDetailedValueFromApi, getFinalWordFormula, getFormulaSources, getSummaryValueFromApi, } from "../../../../../../Utils/Functions/FormulaFunctions";
+import { getFinalWordFormula } from "../../../../../../Utils/Functions/FormulaFunctions";
 import { useGetFormulaDataDetailed } from "../../../../../../Utils/Hooks/Indicator";
-import { dataTypes } from "../../../../../../Utils/Models";
-import CalculationDetails from "../../Index";
 import classes from "./Components/DataSourceCellStyle.module.css";
-import DisplaySource from "./Components/DisplaySourceDataElement";
 import DisplaySourceDataElement from "./Components/DisplaySourceDataElement";
 import DisplaySourceDataSet from "./Components/DisplaySourceDataSet";
 import DisplaySourceProgramDataElementOrAttribute from "./Components/DisplaySourceProgramDataElementOrAttribute";
 import DisplaySourceProgramIndicator from "./Components/DisplaySourceProgramIndicator";
 
-export default function CalculationDetailRow({ formula, location, ...props }:any) {
-    //hooks
-    const engine = useDataEngine();
-    // const updateDataElementHandler = useSetRecoilState(dataElementsStateDictionary);
-    // const updateProgramIndicatorHandler = useSetRecoilState(programIndicatorStateDictionary);
-    // const updateDataSetReportingRatesHandler = useSetRecoilState(dataSetReportingRatesStateDictionary);
+export default function CalculationDetailRow({ formula, location, ...props }: any) {
+  //hooks
+  const engine = useDataEngine();
 
-    const {setValues}=   useContext(DictionaryContext);
-    const { loading, error, data }:any = useGetFormulaDataDetailed(formula, engine, location);
+  const { setValues } = useContext(DictionaryContext);
+  const { loading, error, data }: any = useGetFormulaDataDetailed(formula, engine, location);
 
-    setValues(data);
+  if (loading) {
+    return <Loader text={""} />;
+  }
+  if (error) {
+    return <Error error={error} />;
+  }
 
-    if (loading) {
-        return <Loader text={""}/>;
-    }
-    if (error) {
-        return <Error error={error}/>;
-    }
+  // for store
+  setValues(data);
 
-    // for store
-    // updateDataElementHandler(data?.dataElements);
-    // updateProgramIndicatorHandler(data?.programIndicators);
-    // updateDataSetReportingRatesHandler(data?.dataSetReportingRates);
-
-    return (<>
+  return (
+    <>
       <DataTableCell bordered width={"50%"}>
-        {getFinalWordFormula(formula, data?.dataElements, data?.programIndicators, data?.dataSetReportingRates, data?.attributes, data?.constants, data?.programDtElement, data?.orgUnitCount)}
+        {getFinalWordFormula(
+          formula,
+          data?.dataElements,
+          data?.programIndicators,
+          data?.dataSetReportingRates,
+          data?.attributes,
+          data?.constants,
+          data?.programDtElement,
+          data?.orgUnitCount
+        )}
       </DataTableCell>
       <DataTableCell bordered>
         <div className={classes.sources}>
-          {data?.dataElements?.length > 0 ? (<DisplaySourceDataElement title={"Data Elements"} data={data?.dataElements}/>) : ("")}
-          {data?.dataSetReportingRates?.length > 0 ? (<DisplaySourceDataSet title={"Data Sets"} data={data?.dataSetReportingRates}/>) : ("")}
-          {data?.programIndicators?.length > 0 ? (<DisplaySourceProgramIndicator title={"Program Indicators"} data={data?.programIndicators}/>) : ("")}
-          {data?.programDtElement?.length > 0 ? (<DisplaySourceProgramDataElementOrAttribute title={"Tracker Data Element"} data={data?.programDtElement}/>) : ("")}
-          {data?.attributes?.length > 0 ? (<DisplaySourceProgramDataElementOrAttribute title={"Tracker Data Element"} data={data?.attributes}/>) : ("")}
+          {data?.dataElements?.length > 0 ? <DisplaySourceDataElement title={"Data Elements"} data={data?.dataElements} /> : ""}
+          {data?.dataSetReportingRates?.length > 0 ? <DisplaySourceDataSet title={"Data Sets"} data={data?.dataSetReportingRates} /> : ""}
+          {data?.programIndicators?.length > 0 ? <DisplaySourceProgramIndicator title={"Program Indicators"} data={data?.programIndicators} /> : ""}
+          {data?.programDtElement?.length > 0 ? (
+            <DisplaySourceProgramDataElementOrAttribute title={"Tracker Data Element"} data={data?.programDtElement} />
+          ) : (
+            ""
+          )}
+          {data?.attributes?.length > 0 ? <DisplaySourceProgramDataElementOrAttribute title={"Tracker Data Element"} data={data?.attributes} /> : ""}
         </div>
       </DataTableCell>
-    </>);
+    </>
+  );
 }
 
 CalculationDetailRow.propTypes = {
-    formula: PropTypes.string.isRequired,
-    location: PropTypes.string.isRequired
+  formula: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
 };
-
