fba71166c037f2913ac2855eede20a3761cb96b5
gnnko <nnkogift@hotmail.com>
2022-09-07 16:14:21 +0300

    fix(map thematic layer): Fixes
    - Implemented unique name for each layer

--- src/components/Map/components/MapLayer/components/ThematicLayer/index.tsx
+++ src/components/Map/components/MapLayer/components/ThematicLayer/index.tsx
@@ -1,6 +1,7 @@
 import { CenteredContent, CircularLoader } from "@dhis2/ui";
+import { uid } from "@hisptz/dhis2-utils";
 import { head } from "lodash";
-import React from "react";
+import React, { useRef } from "react";
 import { LayerGroup, LayersControl } from "react-leaflet";
 import Control from "react-leaflet-custom-control";
 import { ThematicLayer as ThematicLayerInterface } from "../../interfaces";
@@ -13,6 +14,7 @@ import useThematicLayerData from "./hooks/config";
 export default function ThematicLayer({ layer }: { layer: ThematicLayerInterface }) {
   const { type, enabled, control, dataItem, name } = layer;
   const { loading, data } = useThematicLayerData(layer);
+  const { current: uniqueName } = useRef<string>(`${name ?? ""}-${uid()}`);
   if (loading) {
     return (
       <LayersControl.Overlay name={name ?? dataItem.displayName} checked={enabled}>
@@ -25,7 +27,7 @@ export default function ThematicLayer({ layer }: { layer: ThematicLayerInterface
 
   return (
     <>
-      <LayersControl.Overlay checked={enabled} name={name ?? dataItem.displayName}>
+      <LayersControl.Overlay checked={enabled} name={uniqueName}>
         <LayerGroup>
           {data?.map((datum) => (type === "choropleth" ? <Choropleth data={datum} key={`${datum?.dataItem?.id}-${datum?.orgUnit?.id}-layer`} /> : null))}
           {data?.map((datum) =>
@@ -35,8 +37,8 @@ export default function ThematicLayer({ layer }: { layer: ThematicLayerInterface
       </LayersControl.Overlay>
       {control && (
         <Control position={control.position}>
-          {type === "choropleth" && <ChoroplethLegend name={name} data={data} dataItem={head(data)?.dataItem ?? dataItem} />}
-          {type === "bubble" && <BubbleLegend name={name} data={data} dataItem={head(data)?.dataItem ?? dataItem} />}
+          {type === "choropleth" && <ChoroplethLegend name={uniqueName} data={data} dataItem={head(data)?.dataItem ?? dataItem} />}
+          {type === "bubble" && <BubbleLegend name={uniqueName} data={data} dataItem={head(data)?.dataItem ?? dataItem} />}
         </Control>
       )}
     </>
