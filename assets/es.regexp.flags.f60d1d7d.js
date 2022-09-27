21cd982dbe16d8c3fce6a6de5f8c83a5e8c19395
nnkogift <nnkogift@users.noreply.github.com>
2022-09-27 11:07:08 +0000

    deploy: 33d3b2ba9cd1b79050be5421af2f00bd91439519

--- assets/es.regexp.flags.f60d1d7d.js
+++
@@ -1,2 +0,0 @@
-import{n as f,q as p,aT as u,aU as d,v as y}from"./iframe.ffe6ac23.js";var E=f,O=p,R=u,b=d,m=y,o=E.RegExp,g=o.prototype,x=O&&m(function(){var r=!0;try{o(".","d")}catch{r=!1}var a={},t="",s=r?"dgimsy":"gimsy",n=function(l,v){Object.defineProperty(a,l,{get:function(){return t+=v,!0}})},e={dotAll:"s",global:"g",ignoreCase:"i",multiline:"m",sticky:"y"};r&&(e.hasIndices="d");for(var i in e)n(i,e[i]);var c=Object.getOwnPropertyDescriptor(g,"flags").get.call(a);return c!==s||t!==s});x&&R(g,"flags",{configurable:!0,get:b});
-//# sourceMappingURL=es.regexp.flags.f60d1d7d.js.map
