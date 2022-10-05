f9d0e18d78719ac5ffd386fcf82a4e13c599a57c
gnnko <nnkogift@hotmail.com>
2022-10-05 17:24:37 +0300

    fix(map): Improvements
    - Improvement of thematic layer popup

--- src/components/Map/components/MapLayer/components/ThematicLayer/components/CustomTooltip/index.tsx
+++ src/components/Map/components/MapLayer/components/ThematicLayer/components/CustomTooltip/index.tsx
@@ -1,23 +1,24 @@
+import i18n from "@dhis2/d2-i18n";
 import React from "react";
 import { Pane, Popup, Tooltip } from "react-leaflet";
+import { useMapPeriods } from "../../../../../MapProvider/hooks";
 import { ThematicLayerData } from "../../../../interfaces";
 
 export default function CustomTooltip({ data: dataObject }: { data: ThematicLayerData }) {
   const { dataItem, orgUnit, data } = dataObject ?? {};
+  const { periods } = useMapPeriods() ?? {};
+
   return (
     <Pane name={`${dataItem.displayName}-${orgUnit.id}-popup-pane`} pane="popupPane">
       <Tooltip>
         {orgUnit?.name} ({data})
       </Tooltip>
       <Popup minWidth={80}>
-        <h3>{orgUnit?.name}</h3>
+        <h3 style={{ margin: 0 }}>{orgUnit?.name}</h3>
+        <div>{dataItem?.displayName}</div>
+        <div>{periods?.map((period) => period.name).join(",")}</div>
         <div>
-          <b>Level: </b>
-          {orgUnit?.level}
-        </div>
-        <div>
-          <b>{dataItem?.displayName}: </b>
-          {data}
+          {i18n.t("Value")}: {data}
         </div>
       </Popup>
     </Pane>
