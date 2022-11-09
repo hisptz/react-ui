22f73e68e81f85d298eec5d2a208b96a31464774
gnnko <nnkogift@hotmail.com>
2022-11-09 12:39:51 +0300

    fix(proxy): Fixes
    - Fixed issues with missing env variables

--- src/proxy.js
+++ src/proxy.js
@@ -5,7 +5,6 @@ config();
 
 const target = process.env.DHIS2_PROXY_SERVER ?? "https://play.dhis2.org/2.38.1.1";
 const port = parseInt(process.env.DHIS2_PROXY_PORT ?? "8080");
-const baseUrl = "";
 
 console.info(`Proxy to ${target} started at ${port}`);
 const proxy = httpProxy.createProxyServer({
