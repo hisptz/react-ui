e9b9a3982dd857522be3c7b0ca4e7b45248059d2
gnnko <nnkogift@hotmail.com>
2022-11-16 21:08:37 +0300

    fix(map config): Fixes
    - Fixed issues with map configuration components

--- src/components/Map/components/EarthEngineLayerConfiguration/EarthEngineLayerConfiguration.stories.tsx
+++ src/components/Map/components/EarthEngineLayerConfiguration/EarthEngineLayerConfiguration.stories.tsx
@@ -2,12 +2,12 @@ import { Story } from "@storybook/react";
 import { EarthEngineLayerConfiguration, EarthEngineLayerConfigurationProps } from "./index";
 import React from "react";
 import { useForm } from "react-hook-form";
-import { CustomGoogleEngineLayer } from "../MapLayer/interfaces";
+import { EarthEngineLayerConfig } from "../MapLayer/interfaces";
 import i18n from "@dhis2/d2-i18n";
 import { Button } from "@dhis2/ui";
 
 const Template: Story<EarthEngineLayerConfigurationProps> = (args) => {
-  const form = useForm<CustomGoogleEngineLayer>();
+  const form = useForm<EarthEngineLayerConfig>();
   return (
     <form className="column gap-16" onSubmit={form.handleSubmit(console.log)}>
       <EarthEngineLayerConfiguration {...args} form={form} />
